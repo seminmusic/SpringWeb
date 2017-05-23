eb1765a2d1a952ed89a00d8072992464eacca452
semin@bamcard-windows <music.semin@gmail.com>
2017-05-23 15:45:01 +0200

    Generate DDL with Hibernate

--- src/main/java/ba/sema/controllers/HomeController.java
+++ src/main/java/ba/sema/controllers/HomeController.java
@@ -3,6 +3,8 @@ package ba.sema.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ba.sema.helpers.HibernateExporter;
+
 
 @Controller
 @RequestMapping(value = "/")
@@ -11,6 +13,9 @@ public class HomeController
 	@RequestMapping(value = "/")
 	public String index()
 	{
+		HibernateExporter exporter = new HibernateExporter("org.hibernate.dialect.PostgreSQLDialect", "ba.sema.entities");
+		exporter.exportToConsole();
+		//
 		return "Home/Index";
 	}
 }
